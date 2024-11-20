f1996ce5d323afa12ebf848631f21db1319a5f94
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-21 00:23:44 +0500

    django-practice

--- home/views.py
+++ home/views.py
@@ -8,6 +8,6 @@ name = 'waleed'
 def home(request):
     return render(request,'home/welcome.html',{'today': datetime.today()})
 
-@login_required
+@login_required(login_url='/admin')
 def autorhized(request):
     return render(request,'home/authorized.html',{})
