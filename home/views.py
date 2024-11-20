5aab8ec734c010773d80e6bc5ba97066cee1ba01
waleed-khan11 <waleed.kn270@gmail.com>
2024-11-21 00:16:37 +0500

    django-practice

--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
+
 name = 'waleed'
 
 def home(request):
     return render(request,'home/welcome.html',{'today': datetime.today()})
+
+@login_required
+def autorhized(request):
+    return render(request,'home/authorized.html',{})
